b0ff6d7cfe1ce70970d5e2920bfde067b54eedc5
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-02-07 16:54:40 +0100

    Implemented gigasecond exercise.

---
+++ gigasecond/src/lib.rs
@@ -0,0 +1,8 @@
+use std::ops::Add;
+use time::PrimitiveDateTime as DateTime;
+use time::Duration;
+
+// Returns a DateTime one billion seconds after start.
+pub fn after(start: DateTime) -> DateTime {
+    start.add(Duration::new(1_000_000_000, 0))
+}
