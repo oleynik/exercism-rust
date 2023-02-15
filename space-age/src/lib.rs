1ee901f03303caa29ecdc8b4fbc4c519cc49c5bb
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-02-15 16:48:34 +0100

    Improved space-age exercise by implementing part of functionality using macros

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -21,52 +21,20 @@ pub trait Planet {
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! planet {
+    ($name: ident, $orbital_period: expr) => {
+        pub struct $name;
+        impl Planet for $name {
+            fn earth_orbital_period() -> f64 {$orbital_period}
+        }
+    }
+}
 
-impl Planet for Mercury {
-    fn earth_orbital_period() -> f64 {
-        0.2408467
-    }
-}
-impl Planet for Venus {
-    fn earth_orbital_period() -> f64 {
-        0.61519726
-    }
-}
-impl Planet for Earth {
-    fn earth_orbital_period() -> f64 {
-        1f64
-    }
-}
-impl Planet for Mars {
-    fn earth_orbital_period() -> f64 {
-        1.8808158
-    }
-}
-impl Planet for Jupiter {
-    fn earth_orbital_period() -> f64 {
-        11.862615
-    }
-}
-impl Planet for Saturn {
-    fn earth_orbital_period() -> f64 {
-        29.447498
-    }
-}
-impl Planet for Uranus {
-    fn earth_orbital_period() -> f64 {
-        84.016846
-    }
-}
-impl Planet for Neptune {
-    fn earth_orbital_period() -> f64 {
-        164.79132
-    }
-}
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Earth, 1f64);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
