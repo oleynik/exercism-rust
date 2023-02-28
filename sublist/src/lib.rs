2cad9a9d161ce23a36d4b80ea2382e1d540eccc7
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-02-28 09:03:06 +0100

    Improved sublist exercise

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -7,47 +7,30 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    let first = _first_list.len();
-    let second = _second_list.len();
-
-    // 0. 1.len == 2.len:   Potentially Equal
-    if first == second
-        && equal(_first_list, _second_list) {
-        return Comparison::Equal;
-    }
-
-    // 1. 1.len < 2.len:    Potentially SubList
-    if first < second
-        && (first == 0 || partially_equal(_first_list, _second_list)) {
-        return Comparison::Sublist;
-    }
-
-    // 2. 1.len > 2.len:    Potentially SuperList
-    if first > second
-        && (second == 0 || partially_equal(_second_list, _first_list)) {
-        return Comparison::Superlist;
-    }
-
-    // 3. Otherwise:        UnEqual
-    Comparison::Unequal
-}
-
-fn equal<T: PartialEq>(first: &[T], second: &[T]) -> bool {
-    for i in 0..first.len() {
-        if first[i] != second[i] {
-            return false
+    match (_first_list.len(), _second_list.len()) {
+        (0, 0) => Comparison::Equal,
+        (0, _) => Comparison::Sublist,
+        (_, 0) => Comparison::Superlist,
+        (m, n) if m > n => {
+            if _first_list.windows(n).any(|v| v == _second_list) {
+                Comparison::Superlist
+            } else {
+                Comparison::Unequal
+            }
         }
-    }
-    true
-}
-
-fn partially_equal<T: PartialEq>(smaller: &[T], bigger: &[T]) -> bool {
-    for i in 0..bigger.len()-smaller.len()+1 {
-        if smaller[0] == bigger[i] {
-            if equal(smaller, &bigger[i..i+smaller.len()]) {
-                return true
+        (m, n) if m < n => {
+            if _second_list.windows(m).any(|v| v == _first_list) {
+                Comparison::Sublist
+            } else {
+                Comparison::Unequal
+            }
+        }
+        (_, _) => {
+            if _first_list == _second_list {
+                Comparison::Equal
+            } else {
+                Comparison::Unequal
             }
         }
     }
-    false
 }
