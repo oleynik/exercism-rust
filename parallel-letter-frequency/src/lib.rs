7ce01234337b2361e0cd0538b9f094f26257195b
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-03-08 18:15:14 +0100

    Implemented parallel-letter-frequency exercise

---
+++ parallel-letter-frequency/src/lib.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+use std::sync::mpsc;
+use std::thread;
+
+fn batch_size(lines_number: usize, group_number: usize) -> usize {
+    if group_number <= 1 {
+        return lines_number;
+    }
+    lines_number / group_number + if lines_number % group_number != 0 {1} else {0}
+}
+
+pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+    if input.len() == 0 {
+        return HashMap::new();
+    }
+    let batch_size = batch_size(input.len(), worker_count);
+    let chunks = input.chunks(batch_size);
+    let mut handlers = vec![];
+    let (send, recv) = mpsc::channel();
+
+    for chunk in chunks {
+        let line = chunk.concat();
+        let sender = send.clone();
+        let handler = thread::spawn(move || {
+            let mut map: HashMap<char, usize> = HashMap::new();
+            line.chars()
+                .filter(|c|c.is_alphabetic())
+                .for_each(|c|{
+                *map.entry(c.to_ascii_lowercase()).or_insert(0) += 1;
+            });
+            sender.send(map).unwrap();
+        });
+        handlers.push(handler)
+    }
+    drop(send);
+
+    let mut result: HashMap<char, usize> = HashMap::new();
+    for map in recv {
+        map.iter().for_each(|(&k, &v)|{
+            *result.entry(k).or_insert(0) += v;
+        });
+    }
+    result
+}
+
