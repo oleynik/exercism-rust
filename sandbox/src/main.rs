664288eace421a3473927ae77564fda161a565c1
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-03-08 18:17:20 +0100

    Added thread communication example in sandbox

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,3 +1,22 @@
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!")
+    let (send, receiver) = mpsc::channel();
+    let mut handlers = vec![];
+
+    for _ in 0..2 {
+        let sender = send.clone();
+        handlers.push(thread::spawn(move || {
+            let thread_id = thread::current().id();
+            sender.send(format!("Hello from {:?}", thread_id)).unwrap();
+        }));
+    }
+    drop(send);
+
+    println!("Ready for messages...");
+    for m in receiver {
+        println!("{:?} | Received: {}", thread::current().id(), m)
+    }
+    println!("Messages Read!");
 }
