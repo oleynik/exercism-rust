4b55d2e01ba5019c18a8c6a8b44a1e552e0050e1
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-03-10 16:09:17 +0100

    Improved proverb exercise by using chain method for combination two iterators

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,9 +1,10 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    match list.is_empty() {
-        true => String::new(),
-        false => list
+    match list.len() {
+        0 => String::new(),
+        _ => list
             .windows(2)
             .map(|item|format!("For want of a {} the {} was lost.\n", item[0], item[1]))
-            .collect::<String>() + format!("And all for the want of a {}.", list.first().unwrap()).as_str()
+            .chain(std::iter::once(format!("And all for the want of a {}.", list[0])))
+            .collect::<String>()
     }
 }
