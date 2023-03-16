79398af150c394903ab62638d84cfd1e1d6abd2e
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-03-16 17:22:13 +0100

    Improved solution of hamming exercise

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -4,13 +4,10 @@ pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
     if s1.len() != s2.len() {
         return None;
     }
-    let mut result = 0;
-    let b1 = s1.as_bytes();
-    let b2 = s2.as_bytes();
-    for i in 0..s1.len() {
-        if b1[i] != b2[i] {
-            result += 1;
-        }
-    }
-    Some(result)
+    Some(
+        s1.chars()
+            .zip(s2.chars())
+            .filter(|(c1, c2)| c1 != c2)
+            .count(),
+    )
 }
