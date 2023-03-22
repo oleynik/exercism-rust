c3f96961e60871f57a75e528745df384756eadae
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-03-22 17:02:00 +0100

    Improved rna-transcription exercise

--- rna-transcription/src/lib.rs
+++ rna-transcription/src/lib.rs
@@ -4,24 +4,23 @@ pub struct Dna(String);
 #[derive(Debug, PartialEq, Eq)]
 pub struct Rna(String);
 
+fn validate_nucleotides(nucleotides: &str, chain: &str) -> Result<String, usize> {
+    chain
+        .to_uppercase()
+        .char_indices()
+        .map(|(i, c)| {
+            if nucleotides.contains(c) {
+                Ok(c)
+            } else {
+                return Err(i);
+            }
+        })
+        .collect()
+}
+
 impl Dna {
-    const NUCLEOTIDES: &'static str = "ACGT";
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        let result = dna
-            .to_uppercase()
-            .char_indices()
-            .map(|(i, c)| {
-                if Self::NUCLEOTIDES.contains(c) {
-                    Ok(c)
-                } else {
-                    Err(i)
-                }
-            })
-            .collect::<Result<String, _>>();
-        match result {
-            Ok(s) => Ok(Dna(s)),
-            Err(i) => Err(i),
-        }
+        validate_nucleotides("ACGT", dna).map(|r| Dna(r))
     }
 
     pub fn into_rna(self) -> Rna {
@@ -29,35 +28,19 @@ impl Dna {
             .0
             .chars()
             .map(|c| match c {
-                'A' => Ok('U'),
-                'C' => Ok('G'),
-                'G' => Ok('C'),
-                'T' => Ok('A'),
-                c => Err(format!("Unknown nucleotide {}", c)),
+                'A' => 'U',
+                'C' => 'G',
+                'G' => 'C',
+                'T' => 'A',
+                c => panic!("Unknown nucleotide {}", c),
             })
-            .filter_map(|r| r.ok())
             .collect();
         Rna::new(rna.as_str()).unwrap()
     }
 }
 
 impl Rna {
-    const NUCLEOTIDES: &'static str = "ACGU";
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        match rna
-            .to_uppercase()
-            .char_indices()
-            .map(|(i, c)| {
-                if Self::NUCLEOTIDES.contains(c) {
-                    Ok(c)
-                } else {
-                    Err(i)
-                }
-            })
-            .collect::<Result<String, _>>()
-        {
-            Ok(s) => Ok(Self(s)),
-            Err(i) => Err(i),
-        }
+        validate_nucleotides("ACGU", rna).map(|r| Rna(r))
     }
 }
