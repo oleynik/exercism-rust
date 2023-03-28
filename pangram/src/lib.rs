e13062db54adbcbb04e94100fec71fd59de21772
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-03-28 16:55:01 +0200

    Implemented pangram exercise

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -3,13 +3,6 @@ pub fn is_pangram(sentence: &str) -> bool {
     if sentence.len() < 26 {
         return false;
     }
-    let mut abc = ('a'..='z').collect::<Vec<char>>();
-    sentence
-        .to_lowercase()
-        .chars()
-        .filter(|c| c.is_alphabetic())
-        .for_each(|c| {
-            abc.binary_search(&c).map(|idx| abc.remove(idx));
-        });
-    abc.is_empty()
+    let lower = sentence.to_lowercase();
+    ('a'..='z').all(|c| lower.contains(c))
 }
