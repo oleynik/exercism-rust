69bea48d9e786c543cc68fd9f36927e1c149e222
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-04-05 17:25:58 +0200

    Start implementation of react exercise.

---
+++ react/src/lib.rs
@@ -0,0 +1,166 @@
+use rand::random;
+use std::collections::HashMap;
+
+/// `InputCellId` is a unique identifier for an input cell.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct InputCellId(u64);
+/// `ComputeCellId` is a unique identifier for a compute cell.
+/// Values of type `InputCellId` and `ComputeCellId` should not be mutually assignable,
+/// demonstrated by the following tests:
+///
+/// ```compile_fail
+/// let mut r = react::Reactor::new();
+/// let input: react::ComputeCellId = r.create_input(111);
+/// ```
+///
+/// ```compile_fail
+/// let mut r = react::Reactor::new();
+/// let input = r.create_input(111);
+/// let compute: react::InputCellId = r.create_compute(&[react::CellId::Input(input)], |_| 222).unwrap();
+/// ```
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct ComputeCellId(u64);
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct CallbackId(u64);
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum CellId {
+    Input(InputCellId),
+    Compute(ComputeCellId),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum RemoveCallbackError {
+    NonexistentCell,
+    NonexistentCallback,
+}
+
+pub struct Reactor<T> {
+    inputs: HashMap<InputCellId, T>,
+    graph: HashMap<CellId, (Vec<CellId>, fn(&[T]) -> T)>,
+}
+
+// You are guaranteed that Reactor will only be tested against types that are Copy + PartialEq.
+impl<T: Copy + PartialEq> Reactor<T> {
+    pub fn new() -> Self {
+        Self {
+            inputs: HashMap::new(),
+            graph: HashMap::new(),
+        }
+    }
+
+    // Creates an input cell with the specified initial value, returning its ID.
+    pub fn create_input(&mut self, _initial: T) -> InputCellId {
+        loop {
+            let i_cell = InputCellId(random());
+            if self.inputs.get(&i_cell).is_none() {
+                self.inputs.insert(i_cell, _initial);
+                return i_cell;
+            }
+        }
+    }
+
+    // Creates a compute cell with the specified dependencies and compute function.
+    // The compute function is expected to take in its arguments in the same order as specified in
+    // `dependencies`.
+    // You do not need to reject compute functions that expect more arguments than there are
+    // dependencies (how would you check for this, anyway?).
+    //
+    // If any dependency doesn't exist, returns an Err with that nonexistent dependency.
+    // (If multiple dependencies do not exist, exactly which one is returned is not defined and
+    // will not be tested)
+    //
+    // Notice that there is no way to *remove* a cell.
+    // This means that you may assume, without checking, that if the dependencies exist at creation
+    // time they will continue to exist as long as the Reactor exists.
+    pub fn create_compute<F: Fn(&[T]) -> T>(
+        &mut self,
+        _dependencies: &[CellId],
+        _compute_func: F,
+    ) -> Result<ComputeCellId, CellId> {
+        // Validation
+        for &cid in _dependencies {
+            match cid {
+                CellId::Input(iid) => {
+                    if self.inputs.get(&iid).is_none() {
+                        return Err(cid);
+                    }
+                }
+                _ => panic!("Unexpected dependency: {:?}", cid),
+            }
+        }
+        loop {
+            let result = ComputeCellId(random());
+            let cell_id = CellId::Compute(result);
+            if self.graph.get(&cell_id).is_none() {
+                self.graph
+                    .insert(cell_id, (_dependencies.to_vec(), _compute_func));
+                return Ok(result);
+            }
+        }
+    }
+
+    // Retrieves the current value of the cell, or None if the cell does not exist.
+    //
+    // You may wonder whether it is possible to implement `get(&self, id: CellId) -> Option<&Cell>`
+    // and have a `value(&self)` method on `Cell`.
+    //
+    // It turns out this introduces a significant amount of extra complexity to this exercise.
+    // We chose not to cover this here, since this exercise is probably enough work as-is.
+    pub fn value(&self, id: CellId) -> Option<T> {
+        match id {
+            CellId::Input(iid) => self.inputs.get(&iid).map(|e| *e),
+            _ => None,
+        }
+    }
+
+    // Sets the value of the specified input cell.
+    //
+    // Returns false if the cell does not exist.
+    //
+    // Similarly, you may wonder about `get_mut(&mut self, id: CellId) -> Option<&mut Cell>`, with
+    // a `set_value(&mut self, new_value: T)` method on `Cell`.
+    //
+    // As before, that turned out to add too much extra complexity.
+    pub fn set_value(&mut self, _id: InputCellId, _new_value: T) -> bool {
+        match self.inputs.get_mut(&_id) {
+            Some(v) => Some(*v = _new_value).is_some(),
+            _ => false,
+        }
+    }
+
+    // Adds a callback to the specified compute cell.
+    //
+    // Returns the ID of the just-added callback, or None if the cell doesn't exist.
+    //
+    // Callbacks on input cells will not be tested.
+    //
+    // The semantics of callbacks (as will be tested):
+    // For a single set_value call, each compute cell's callbacks should each be called:
+    // * Zero times if the compute cell's value did not change as a result of the set_value call.
+    // * Exactly once if the compute cell's value changed as a result of the set_value call.
+    //   The value passed to the callback should be the final value of the compute cell after the
+    //   set_value call.
+    pub fn add_callback<F: FnMut(T)>(
+        &mut self,
+        _id: ComputeCellId,
+        _callback: F,
+    ) -> Option<CallbackId> {
+        unimplemented!()
+    }
+
+    // Removes the specified callback, using an ID returned from add_callback.
+    //
+    // Returns an Err if either the cell or callback does not exist.
+    //
+    // A removed callback should no longer be called.
+    pub fn remove_callback(
+        &mut self,
+        cell: ComputeCellId,
+        callback: CallbackId,
+    ) -> Result<(), RemoveCallbackError> {
+        unimplemented!(
+            "Remove the callback identified by the CallbackId {callback:?} from the cell {cell:?}"
+        )
+    }
+}
